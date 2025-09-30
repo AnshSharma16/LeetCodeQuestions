fa1e0d746d2862676676eadae87d5f5d0deed1a2
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2025-09-30 16:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (15.87%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+        int[] temp = new int[m + n];  // temporary array
+        int i = 0, j = 0, k = 0;
+
+        // Merge both arrays into temp[]
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
         }
-        Arrays.sort(nums1);
+
+        // Copy remaining elements from nums1
+        while (i < m) temp[k++] = nums1[i++];
+
+        // Copy remaining elements from nums2
+        while (j < n) temp[k++] = nums2[j++];
+
+        // Copy temp back to nums1
+        for (int idx = 0; idx < m + n; idx++) {
+            nums1[idx] = temp[idx];
+        }
     }
 }
-
